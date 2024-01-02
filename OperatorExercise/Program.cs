72243e5067b168644291f7209fadc838ebbb9fad
Elijah Blair <elijahblair2005@gmail.com>
2024-01-02 11:40:12 -0500

    complete

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            var sum = a + b;
+
+            var sub = a - b;
+
+            var mult = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+            
+            Console.WriteLine($"{a}+{b}={sum}, {a}-{b}={sub}, {a}*{b}={mult}, {a}/{b} is {quotient} remainder {remainder}");
         }
     }
 }
